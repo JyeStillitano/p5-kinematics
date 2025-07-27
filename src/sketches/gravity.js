cfb2a090544e5de7520701443eb94d3666006e5e
Jye J. Stillitano <jyestillitano@hotmail.com>
2025-07-27 13:11:58 +1000

    Gravity mobile styling updated.

---
+++ src/sketches/gravity.js
@@ -0,0 +1,59 @@
+import { limitDistance } from '../util/limitDistance.js'
+
+export const gravity = (p5) => {
+  const d = window.innerWidth > 500 ? 75 : 25
+  const numLinks = 12
+  let anchor
+  let links = []
+  let gravityVector
+  let gravityScale = 15
+  let gravity = true
+
+  function drawJoint(x, y, r) {
+    p5.fill(0, 0)
+    p5.strokeWeight(2)
+    p5.circle(x, y, r)
+    p5.strokeWeight(10)
+    p5.circle(x, y, 1)
+  }
+
+  p5.setup = () => {
+    p5.createCanvas(window.innerWidth, window.innerHeight)
+    p5.noCursor()
+
+    for (let i = 0; i < numLinks; i++) {
+      links.push(p5.createVector(0, 0))
+    }
+
+    anchor = p5.createVector(0, 0)
+    gravityVector = p5.createVector(0, gravityScale)
+  }
+
+  p5.draw = () => {
+    // Draw Background
+    p5.background(220)
+
+    // Input
+    anchor.set(p5.mouseX, p5.mouseY)
+
+    // ============= Anchor =================
+    // Draw Anchor
+    drawJoint(anchor.x, anchor.y, d * 2)
+
+    // =============== Link =================
+    // Limit Distance
+    for (let i = 0; i < numLinks; i++) {
+      let l = links[i]
+      let p = i === 0 ? anchor : links[i - 1]
+
+      // Apply Gravity
+      if (gravity) l.add(gravityVector)
+
+      // Limit Distance
+      limitDistance(l, p, d)
+
+      // Draw
+      drawJoint(l.x, l.y, d * 2)
+    }
+  }
+}
